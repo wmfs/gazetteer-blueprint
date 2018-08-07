260d79d8dbb3e1e1e792a4bdfc773e1f40bd964e
reecebrend <reece.brend@wmfs.net>
2018-08-07 12:54:09 +0100

    fix(gazetteerModel): If we use find where uprn equals blah then handle returned array

--- functions/get-address-label.js
+++ functions/get-address-label.js
@@ -3,7 +3,7 @@
 module.exports = function getAddressLabel () {
   return async function getAddressLabel (env, event) {
     const gazetteerModel = env.bootedServices.storage.models['wmfs_gazetteer']
-    const gazetteerRecord = await gazetteerModel.findById(event.boardKeys.uprn)
-    return {addressLabel: gazetteerRecord.addressLabel || ''}
+    const gazetteerRecord = await gazetteerModel.find({where: {uprn: {equals: event.boardKeys.uprn}}})
+    return {addressLabel: gazetteerRecord[0].addressLabel || ''}
   }
 }
