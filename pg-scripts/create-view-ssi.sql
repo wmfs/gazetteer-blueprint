38d49605ed55298f2203466ef8b61cd3f7a3f1df
Meena.Hoda <meena.hoda@wmfs.net>
2019-09-24 08:48:32 +0100

    feat: ssi flattened view to have modified/created timestamps

--- pg-scripts/create-view-ssi.sql
+++ pg-scripts/create-view-ssi.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW wmfs.ssi_flattened AS
-SELECT uprn, category, information FROM wmfs.ssi
+SELECT uprn, category, information, _modified, _created FROM wmfs.ssi
 UNION
-SELECT a.uprn, b.category, b.information FROM wmfs.ssi_link a, wmfs.ssi b WHERE a.parent_uprn = b.uprn;
+SELECT a.uprn, b.category, b.information, a._modified, a._created FROM wmfs.ssi_link a, wmfs.ssi b WHERE a.parent_uprn = b.uprn;
