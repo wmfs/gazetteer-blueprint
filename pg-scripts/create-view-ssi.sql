bbbcb1a189e4044e774c3487164831cce168bd6b
Meena.Hoda <meena.hoda@wmfs.net>
2019-09-30 08:58:57 +0100

    refactor: ssi tables so pk is uprn, category

--- pg-scripts/create-view-ssi.sql
+++ pg-scripts/create-view-ssi.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW wmfs.ssi_flattened AS
-SELECT uprn, category, information, _modified, _created FROM wmfs.ssi
-UNION
-SELECT a.uprn, b.category, b.information, a._modified, a._created FROM wmfs.ssi_link a, wmfs.ssi b WHERE a.parent_uprn = b.uprn;
+--CREATE OR REPLACE VIEW wmfs.ssi_flattened AS
+--SELECT uprn, category, information, _modified, _created FROM wmfs.ssi
+--UNION
+--SELECT a.uprn, b.category, b.information, a._modified, a._created FROM wmfs.ssi_link a, wmfs.ssi b WHERE a.parent_uprn = b.uprn;
