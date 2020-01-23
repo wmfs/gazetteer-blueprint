11e8abf59ec17d7f3accce764f23a3759979fb76
Meena.Hoda <meena.hoda@wmfs.net>
2020-01-23 09:09:17 +0000

    feat: add marked as invalid flag to gazetteer table and strip from vision extract [ch347]

--- functions/ssi-line-break-cleaner.js
+++ functions/ssi-line-break-cleaner.js
@@ -1,12 +1,14 @@
 function ssiLineBreakCleaner (ctx) {
   return function ssiLineBreakCleaner (sourceRow, callback) {
     if (sourceRow[0] === 67 && sourceRow[4] !== null) {
-      const category = sourceRow[4]
-      const information = sourceRow[5]
+      const category = sourceRow[4].replace(/\r/g, '').replace(/\n/g, '/r/n')
+      const information = sourceRow[5].replace(/\r/g, '').replace(/\n/g, '/r/n')
 
-      const notes = `${category}:/r/n${category.replace(/./g, '-')}/r/n${information}`
+      const notes = `${category}:/r/n${category.replace(/\./g, '-')}/r/n${information}`
 
       sourceRow[4] = notes
+      sourceRow.pop()
+    } else if (sourceRow[0] === 65) {
       sourceRow.pop()
     }
 
