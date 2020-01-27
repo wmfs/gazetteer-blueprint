14a9d9fd2e376f9be649d94723c56e3a5984d1ce
Meena.Hoda <meena.hoda@wmfs.net>
2020-01-27 15:24:45 +0000

    style: standard --fix

--- functions/get-address-label.js
+++ functions/get-address-label.js
@@ -2,7 +2,7 @@
 
 module.exports = function getAddressLabel () {
   return async function getAddressLabel (env, event) {
-    const gazetteerModel = env.bootedServices.storage.models['wmfs_gazetteer']
+    const gazetteerModel = env.bootedServices.storage.models.wmfs_gazetteer
     const gazetteerRecord = await gazetteerModel.find({ where: { uprn: { equals: event.boardKeys.uprn } } })
     return { addressLabel: gazetteerRecord[0].addressLabel || '' }
   }
