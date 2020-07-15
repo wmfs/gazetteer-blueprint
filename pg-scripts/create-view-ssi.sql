0031e50cd46df420709831e3607f7d572816ea93
Meena.Hoda <meena.hoda@wmfs.net>
2020-07-15 14:53:31 +0100

    feat: create view grouped by uprn, joining category and information into notes [ch1186]

--- pg-scripts/create-view-ssi.sql
+++ pg-scripts/create-view-ssi.sql
@@ -1,4 +1,18 @@
 CREATE OR REPLACE VIEW wmfs.ssi_flattened AS
-SELECT uprn, category, information, _modified, _created FROM wmfs.ssi
+SELECT uprn, category, information, _modified, _created
+FROM wmfs.ssi
 UNION
-SELECT a.uprn, a.parent_category, b.information, a._modified, a._created FROM wmfs.ssi_link a, wmfs.ssi b WHERE a.parent_uprn = b.uprn AND a.parent_category = b.category;
+SELECT a.uprn, a.parent_category, b.information, a._modified, a._created
+FROM wmfs.ssi_link a, wmfs.ssi b
+WHERE a.parent_uprn = b.uprn
+AND a.parent_category = b.category;
+
+
+CREATE OR REPLACE VIEW wmfs.ssi_aggregated AS
+SELECT
+uprn,
+STRING_AGG(category || E'\n' || REGEXP_REPLACE(category, '.', '-', 'g') || E'\n' || information, E'\n\n') AS notes,
+_modified,
+_created
+FROM wmfs.ssi_flattened
+GROUP BY uprn, _modified, _created;
