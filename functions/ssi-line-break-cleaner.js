2b6104b0a2e23227c659c5266597094ec49fb4c0
Meena.Hoda <meena.hoda@wmfs.net>
2020-07-15 14:59:51 +0100

    feat: adjust ssi link break cleaner for new view

--- functions/ssi-line-break-cleaner.js
+++ functions/ssi-line-break-cleaner.js
@@ -1,13 +1,15 @@
 function ssiLineBreakCleaner (ctx) {
   return function ssiLineBreakCleaner (sourceRow, callback) {
     if (sourceRow[0] === 67 && sourceRow[4] !== null) {
-      const category = sourceRow[4].replace(/\r/g, '').replace(/\n/g, '/r/n')
-      const information = sourceRow[5].replace(/\r/g, '').replace(/\n/g, '/r/n')
+      sourceRow[4] = sourceRow[4].replace(/\r/g, '').replace(/\n/g, '/r/n')
 
-      const notes = `${category}:/r/n${category.replace(/./g, '-')}/r/n${information}`
-
-      sourceRow[4] = notes
-      sourceRow.pop()
+      // const category = sourceRow[4].replace(/\r/g, '').replace(/\n/g, '/r/n')
+      // const information = sourceRow[5].replace(/\r/g, '').replace(/\n/g, '/r/n')
+      //
+      // const notes = `${category}:/r/n${category.replace(/./g, '-')}/r/n${information}`
+      //
+      // sourceRow[4] = notes
+      // sourceRow.pop()
     }
 
     callback(null, sourceRow)
