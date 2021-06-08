b889fa43951c8ab3f7204140b43301ff139082de
meenahoda <meena.hoda@wmfs.net>
2021-06-08 16:50:41 +0100

    fix: remove counter from streets

--- functions/exclude-street-records.js
+++ functions/exclude-street-records.js
@@ -3,6 +3,7 @@ module.exports = function excludeStreetRecords () {
     if (sourceRow[0] === 61) { // street record
       sourceRow.pop() // strip marked_as_invalid
       sourceRow.pop() // strip exclude_from_vision_extract
+      sourceRow.pop() // counter
     }
 
     callback(null, sourceRow)
