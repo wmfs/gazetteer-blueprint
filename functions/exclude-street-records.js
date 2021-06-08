fb21b7d472ba21814773c044cc4ce253bc852941
meenahoda <meena.hoda@wmfs.net>
2021-06-08 15:48:20 +0100

    feat: ability to mark streets as invalid and exclude from export

---
+++ functions/exclude-street-records.js
@@ -0,0 +1,10 @@
+module.exports = function excludeStreetRecords () {
+  return function excludeStreetRecords (sourceRow, callback) {
+    if (sourceRow[0] === 61) { // street record
+      sourceRow.pop() // strip marked_as_invalid
+      sourceRow.pop() // strip exclude_from_vision_extract
+    }
+
+    callback(null, sourceRow)
+  }
+}
